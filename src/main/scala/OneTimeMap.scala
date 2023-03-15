b7ac7cf6bc6d1037d61279b8fbc7e7521bd04e4c
Pavel Perikov <perikov@gmail.com>
2023-03-15 19:41:20 +0400

    MapLock

--- src/main/scala/OneTimeMap.scala
+++
@@ -1,7 +0,0 @@
-package com.perikov.scratchlink
-
-
-trait OneTimeMap[F[_], K, V]:
-  def get(k: K): F[Option[V]]
-  def put(k: K, v: V): F[Boolean]
-  def has(k: K): F[Boolean]
---
+++ src/main/scala/plans/MapLock.scala
@@ -0,0 +1,51 @@
+package com.perikov.scratchlink.plans
+
+trait MapLock[F[_], K, V]:
+  /** @param action
+    *   action to be performed if the key is reserved successfully. for example,
+    *   sending a request
+    * @return
+    *   [[None]] if the key cannot be reserved, e.g. it is already reserved
+    *   [[Some(v)]] the value after [[fulfill]] is called
+    */
+  def reserve(action: F[Unit])(key: K): F[Option[V]]
+
+  /** @return
+    *   [[true]] if the key was present and the value was fulfilled
+    */
+  def fulfill(key: K, value: V): F[Boolean]
+
+object MapLock:
+  import cats.*
+  import cats.data.OptionT
+  import cats.implicits.*
+  import cats.effect.*
+  import cats.effect.implicits.*
+
+  def apply[F[_], K, V](using F: MapLock[F, K, V]): MapLock[F, K, V] = F
+
+  def create[K, V, F[_]: Concurrent]: F[MapLock[F, K, V]] =
+    type M = Map[K, V]
+    type D = Deferred[F, V]
+    Ref
+      .of(Map.empty[K, D])
+      .map(refMap =>
+        
+        new MapLock[F, K, V]:
+
+          override def reserve(action: F[Unit])(key: K): F[Option[V]] =
+            def processDeferred(d: D) = d.get.guarantee(refMap.update(_ - key))
+            val allocDeferred: F[Option[Deferred[F, V]]]         =
+              Deferred[F, V].flatMap(d =>
+                refMap.modify { m =>
+                  if m.contains(key) then (m, none)
+                  else (m + (key -> d), d.some)
+                }
+              )
+            OptionT(allocDeferred).semiflatMap(processDeferred).value
+          override def fulfill(key: K, value: V): F[Boolean]          =
+            OptionT(refMap.getAndUpdate(_ - key).map(_.get(key)))
+              .semiflatMap(_.complete(value))
+              .getOrElse(false)
+      )
+end MapLock
