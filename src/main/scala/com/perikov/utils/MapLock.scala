6afeae797fddd7726986fb3cb448438dcbf19702
Pavel Perikov <perikov@gmail.com>
2023-03-17 00:13:18 +0400

    fix processing action

--- src/main/scala/com/perikov/utils/MapLock.scala
+++ src/main/scala/com/perikov/utils/MapLock.scala
@@ -8,17 +8,16 @@ trait MapLock[F[_], K, V]:
     *   [[None]] if the key cannot be reserved, e.g. it is already reserved
     *   [[Some(v)]] the value after [[fulfill]] is called
     */
-  def reserve(action: F[Unit])(key: K): F[Option[V]]
+  def reserve(key: K, action: F[Unit]): F[Option[V]]
 
   /** the key is removed if it is present
-    * 
-   * @return
+    *
+    * @return
     *   [[true]] if the key was present and the value was fulfilled
-    * 
     */
   def fulfill(key: K, value: V): F[Boolean]
 
-object MapLock: 
+object MapLock:
   import cats.*
   import cats.data.OptionT
   import cats.implicits.*
@@ -35,19 +34,21 @@ object MapLock:
     Ref
       .of(Map.empty[K, D])
       .map(refMap =>
-        
         new MapLock[F, K, V]:
 
-          override def reserve(action: F[Unit])(key: K): F[Option[V]] =
-            def processDeferred(d: D) = d.get.guarantee(refMap.update(_ - key))
-            val allocDeferred: F[Option[Deferred[F, V]]]         =
+          override def reserve(key: K, action: F[Unit]): F[Option[V]] =
+            def processDeferred(d: D)                    = d.get.guarantee(refMap.update(_ - key))
+            val allocDeferred: F[Option[Deferred[F, V]]] =
               Deferred[F, V].flatMap(d =>
-                refMap.modify ( m =>
+                refMap.modify(m =>
                   if m.contains(key) then (m, none)
                   else (m + (key -> d), d.some)
                 )
               )
-            OptionT(allocDeferred).semiflatMap(processDeferred).value
+            OptionT(allocDeferred)
+              .semiflatMap(a => action.as(a))
+              .semiflatMap(processDeferred)
+              .value
           override def fulfill(key: K, value: V): F[Boolean]          =
             OptionT(refMap.getAndUpdate(_ - key).map(_.get(key)))
               .semiflatMap(_.complete(value))
